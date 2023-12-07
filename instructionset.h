eb618c3cd337ac1b802d49e4b87b6921f71ce1fe
Joshua F <97619681+JoshuaFarmer@users.noreply.github.com>
2023-12-07 19:12:25 +0000

    Update instructionset.h

--- instructionset.h
+++ instructionset.h
@@ -13,9 +13,13 @@ char i_set[5] = {
     Mov r1, 1
 */
 
+// added by josh
+// example:
+// 0x3, 0x1, 0x1 (set R1 to 1)
 typedef enum {
     // added no-op as its useful
     INST_NOP=0,
     INST_ADD=1,
     INST_SUB=2,
+    INST_MOV=3,
 } instructions;
