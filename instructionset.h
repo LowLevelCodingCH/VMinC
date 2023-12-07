8fdfbea706eb20250f88edb74042b148223f26ad
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2023-12-07 22:19:42 +0300

    Update instructionset.h

--- instructionset.h
+++ instructionset.h
@@ -1,15 +1,9 @@
-char i_set[5] = {
-    0x01, 0x02, 0x03, 0x04, 0x05
-};
 
 /*
-    Mov r1; Add r1; Sub r1; Mul r1; Div r1
-
-
     Basic program:
     (Adds 1 to the r1 register.)
-    01 01 
-    00000001 00000001
+    03 01 
+    00000003 00000001
     Mov r1, 1
 */
 
