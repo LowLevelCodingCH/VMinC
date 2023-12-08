cc5fb0413b43b601a028d23af21d2cbd334a1cd9
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2023-12-08 21:57:13 +0300

    Update bios.asm

--- bios.asm
+++ bios.asm
@@ -1,6 +1,8 @@
 INT10H:
+    CMP R4, 0x0E
+    JZ DISPLAYCHAR
+    JMP CONTINUE01
     DISPLAYCHAR:
-        CMP R4, 0x0E ;AH
         ADD R1, 0xFFC0
         C1T2
         SUB R2, 0xFFC0
@@ -12,10 +14,14 @@ INT10H:
         CDO R3
         NOP
         DEL R3
+    CONTINUE01:
+        NOP
 
 INT15H:
+    CMP R4, 0x86
+    JZ WAITSEC
+    JMP CONTINUE02
     WAITSEC:
-        CMP R4, 0x86
         CALL WAITTIME
         WAITTIME:
             CMP R5, R6
@@ -23,7 +29,14 @@ INT15H:
             NOP
         END15H:
             NOP
+    CONTINUE02:
+        NOP
 
 INT16H:
+    CMP R4, 0x00
+    JZ READCHAR
+    JMP CONTINUE03
     READCHAR:
         KGI R5
+    CONTINUE03:
+        NOP
