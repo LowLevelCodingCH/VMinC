ab0d705424ad7ab781fc4defb9084a71f057e44a
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2023-12-08 14:45:19 +0300

    NOW INT1H

--- bios.h
+++ bios.h
@@ -5,7 +5,7 @@
 struct interrupts{
     // i_set = isetvar
 
-    struct int10h {
+    struct int1h {
         // display the "CHAR" in r1 to the screen
         putc(cpu.registers.R1); // josh: ALWAYS REMEMBER SEMICOLONS // llcch: OKAY SORRY
     };
