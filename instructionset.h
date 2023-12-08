d6e7329320aeeb4e307551c2b43c2667d5053d32
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2023-12-08 14:43:58 +0300

    Update instructionset.h ;; INST_LDI...

--- instructionset.h
+++ instructionset.h
@@ -11,9 +11,16 @@
 // example:
 // 0x3, 0x1, 0x1 (set R1 to 1)
 typedef enum {
-    // added no-op as its useful
+    // added no-op as its useful // llcch: added some more loads
     INST_NOP=0,
     INST_ADD=1,
     INST_SUB=2,
     INST_LDI1=3,
+    INST_LDI2=4,
+    INST_LDI3=5,
+    INST_LDI4=6,
+    INST_LDI5=7
+    INST_LDI6=8
+    INST_LDI7=9,
+    INST_LDI8=10,
 } instructions;
