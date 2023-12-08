16873c461f8848e2bcb206691ca4e0d46276da13
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2023-12-08 21:44:57 +0300

    Update bios.asm

--- bios.asm
+++ bios.asm
@@ -1,12 +1,27 @@
-#include "instructionset.h"
-#include "cpu.h"
-#include <stdio.h>
+INT10H:
+    DISPLAYCHAR:
+        CMP R4, 0x0E ;AH
+        ADD R1, 0xFFC0
+        C1T2
+        SUB R2, 0xFFC0
+        ; now getting ready: DISPLAY CHARACTER IN R3
+        C2T3
+        NOP
+        DEL R1
+        DEL R2
+        CDO R3
+        NOP
+        DEL R3
 
-struct interrupts{
-    // i_set = isetvar
+INT15H:
+    WAITSEC:
+        CMP R4, 0x86
+        CALL WAITTIME
+        WAITTIME:
+            CMP R5, R6
+            JZ END15H
+            NOP
 
-    char int10h {
-        // display the "CHAR" in r1 to the screen
-        printf("%s", R1);
-    };
-};
+INT16H:
+    READCHAR:
+        KGI R5
