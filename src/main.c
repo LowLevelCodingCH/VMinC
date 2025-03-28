c06da353e0afb832ac909bb466323f45ad7e2a37
TadaHrd <hrudka.tadeas@gmail.com>
2025-03-28 23:57:21 +0100

    change main code

--- src/main.c
+++ src/main.c
@@ -5,16 +5,52 @@
 #include "charset.c"
 
 int main(int argc, char *argv[]) {
-    if (init_SDL("works omg", 640, 480))
-        return 1;
+    G_initSDL("works lessgo", 640, 480);
 
     // example sdl code
-    setDrawColor(0x00FF00FF);
-    for (int i = 1; i < 479; i++) {
-        setPixel(i, i);
-        setPixel(i + 1, i);
-        setPixel(i, i + 1);
+    SDL_Texture *videomem = SDL_CreateTexture(G_SDLrenderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, 640, 480);
+
+    if (!videomem)
+        G_error();
+
+    if (SDL_SetRenderTarget(G_SDLrenderer, videomem))
+        G_error();
+
+    G_setDrawColor(0x00FF00FF);
+    
+    int xOffset = 0, yOffset = 0; 
+
+    for (int i = 0; i < 96; i++) {
+        for (int j = 0; j < 16; j++) {
+            for (int k = 0; k < 16; k++) {
+                G_setDrawColor(0x00FF00FF * charset_debug[i][j + k * 16]);
+
+                G_setPixel(j + xOffset * 16, k + yOffset * 16);
+            }
+        }
+        if (xOffset++ == 40) {
+            xOffset = 0;
+            yOffset++;
+        }
     }
+
+    yOffset += 2;
+    xOffset = 0;
+
+    for (int i = 0; i < 96; i++) {
+        for (int j = 0; j < 16; j++) {
+            for (int k = 0; k < 16; k++) {
+                G_setDrawColor(0x00FF00FF * charset[i][j + k * 16]);
+
+                G_setPixel(j + xOffset * 16, k + yOffset * 16);
+            }
+        }
+        if (xOffset++ == 40) {
+            xOffset = 0;
+            yOffset++;
+        }
+    }
+
     // end example sdl code
 
     // Execution loop
@@ -22,15 +58,38 @@ int main(int argc, char *argv[]) {
         // run instruction
 
         SDL_Event e;
-        SDL_WaitEventTimeout(&e, 0);
+        if (!SDL_WaitEventTimeout(&e, 1000/60))
+            G_error();
 
         // User requests quit
         if (e.type == SDL_QUIT)
             break;
+        
+        if (e.type == SDL_KEYDOWN)
+            {} // handle keydown
+        if (e.type == SDL_KEYUP)
+            {} // handle keyup
 
         // Update screen
-        SDL_RenderPresent(sdl_renderer);
+        G_strechScreen();
+
+        if (SDL_RenderCopy(G_SDLrenderer, videomem, NULL, NULL))
+            G_error();
+
+        SDL_RenderPresent(G_SDLrenderer);
+
+        if (SDL_SetRenderTarget(G_SDLrenderer, NULL))
+            G_error();
+
+        // Draw the texture
+        if (SDL_RenderCopy(G_SDLrenderer, videomem, NULL, NULL))
+            G_error();
+
+        // Draw on the screen
+        SDL_RenderPresent(G_SDLrenderer);
     }
 
-    quit_sdl();
+    SDL_DestroyTexture(videomem);
+
+    G_quitSDL();
 }
