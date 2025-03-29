e05dab5dc1a88119932c1e3a1ac854b7a109c8e0
TadaHrd <hrudka.tadeas@gmail.com>
2025-03-29 11:18:28 +0100

    document sdl_graphics

--- src/graphics/sdl_graphics.h
+++ src/graphics/sdl_graphics.h
@@ -5,16 +5,33 @@
 #include <SDL.h>
 #include "sdl_init.h"
 
+/**
+ * Sets a pixel at the current render target.
+ * 
+ * @param x x position
+ * @param y y position
+ */
 void G_setPixel(int x, int y) {
     if (SDL_RenderDrawPoint(G_SDLrenderer, x, y))
         G_error();
 }
 
+/**
+ * Sets the global draw color.
+ * 
+ * The color is reset when i.e. the screen rendering routine is ran (it clears the screen).
+ * 
+ * @param x x position
+ * @param y y position
+ */
 void G_setDrawColor(uint32_t color) {
     if (SDL_SetRenderDrawColor(G_SDLrenderer, (color & 0xFF000000) >> 24, (color & 0x00FF0000) >> 16, (color & 0x0000FF00) >> 8, color & 0x000000FF))
         G_error();
 }
 
+/**
+ * Sets rendering scale such that the video memory takes up the whole window.
+ */
 void G_strechScreen() {
     int width, height;
     SDL_GetWindowSize(G_SDLwindow, &width, &height);
