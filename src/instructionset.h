a4d7217db6fb9e322ee0de24e4452afabdc19e83
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2025-03-29 09:34:55 +0100

    changed iset and made a good example

--- src/instructionset.h
+++ src/instructionset.h
@@ -19,26 +19,28 @@ typedef enum {
     INST_BSL,
     INST_CMP,
 
-    LR1,   // Load into registers (1-8)
-    LR2,   // Load immediates btw
-    LR3,
-    LR4,
-    LR5,
-    LR6,
-    LR7,
-    LR8,
+    INST_LR1,      // Load into registers (1-8)
+    INST_LR2,      // Load immediates btw
+    INST_LR3,
+    INST_LR4,
+    INST_LR5,
+    INST_LR6,
+    INST_LR7,
+    INST_LR8,
 
-    RCPY, // Copy from a memory address stored in a register into a register (Reverse copy)
-	   // Example
-	   //     rcpy r1, r2 ; Copy into r1 the value that is in ram at address of the value r2
+    INST_INT,      // Interrupt
 
-    CPYR, // Copy registers
-	  // Example
-	  //     cpyr r1, r2 ; Copy into r1 the value that r2 posesses (I speak premium English)
+    INST_RCPY,     // Copy from a memory address stored in a register into a register (Reverse copy)
+                   // Example
+                   //     rcpy r1, r2 ; Copy into r1 the value that is in ram at address of the value r2
 
-    CPY, // Copy data from one register to a memory address (specified by another register)
-         // Example:
-         //     cpy r1, r2   ; Copy into the memory address specified by r1 the value of r2
+    INST_CPYR,     // Copy registers
+                   // Example
+                   //     cpyr r1, r2 ; Copy into r1 the value that r2 posesses (I speak premium English)
+
+    INST_CPY,      // Copy data from one register to a memory address (specified by another register)
+                   // Example:
+                   //     cpy r1, r2   ; Copy into the memory address specified by r1 the value of r2
 
     INST_JMP,
     INST_JC,
--- src/vm.h
+++ src/vm.h
@@ -1,5 +1,7 @@
 #ifndef VM_h_
 #define VM_h_
+
+#include "cpu.h"
 #include "instructionset.h"
 
 #endif
